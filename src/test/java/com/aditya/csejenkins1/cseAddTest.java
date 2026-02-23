0165e0d76f1f29f90dd4d78f1acfa529b39dd2ce
Priyanka05f0 <lakshmipriyankabethampudi.23@gmail.com>
2026-02-24 00:17:02 +0530

    2nd commit

--- src/test/java/com/aditya/csejenkins1/cseAddTest.java
+++ src/test/java/com/aditya/csejenkins1/cseAddTest.java
@@ -1,24 +1,21 @@
 package com.aditya.csejenkins1;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class cseAddTest {
-cseAdd c = new cseAdd();
-	@Test
-	void csetest() {
-		int result = c.add(2,3);
-		assertEquals(5,result);
-				
-	}
-	
-	@Test
-	void csetest() {
-		int result = c.mul(2,3);
-		assertEquals(6,result);
+class CseAddTest {
+
+    cseAdd c = new cseAdd();
+
+    @Test
+    void addTest() {
+        int result = c.add(2,3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void mulTest() {
+        int result = c.mul(2,3);
+        assertEquals(6, result);
+    }
 }
